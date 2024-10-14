471624c825f8416975cff65543a0537d944822c8
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2024-10-14 01:26:40 -0400

    passes all tests

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = array => array.slice(0, 2);
+const returnLastTwoDrivers = array => array.slice(array.length - 2);
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = (n) => (fare) => fare * n;
+const fareDoubler = fare => fare * 2;
+const fareTripler = fare => fare * 3;
+const selectDifferentDrivers = (array, functionName) => functionName(array);
